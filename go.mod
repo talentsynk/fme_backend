2b7e87b438e478f4bf9fc2a2ff9894ad5c72d6aa
delsahim <delsahim@gmail.com>
2024-02-24 06:54:14 +0100

    Initia setup with env reading configuration

---
+++ go.mod
@@ -0,0 +1,5 @@
+module fme_backend
+
+go 1.18
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ internal/config/config..go
@@ -0,0 +1,35 @@
+package config
+
+import (
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+)
+
+type Config struct {
+    // Configuration Variables 
+    DatabaseURL string
+    
+}
+
+var AppConfig *Config
+
+func init() {
+    // Load .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Initializing AppConfig with values from .env file
+    AppConfig = &Config{
+        DatabaseURL: os.Getenv("DATABASE_URL"),
+        
+    }
+}
+
+// GetDatabaseURL returns the configured database URL
+func GetDatabaseURL() string {
+    return AppConfig.DatabaseURL
+}
+ 
