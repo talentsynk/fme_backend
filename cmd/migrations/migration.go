38091eb0473effcd18aed8fcabd945a84fdc35b8
delsahim <delsahim@gmail.com>
2024-02-26 11:58:25 +0100

    initial setup of user model

---
+++ cmd/migrations/migration.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fme_backend/internal/config"
+	"fme_backend/internal/user"
+)
+
+func init() {
+	config.ConnectToDb()
+}
+
+func main() {
+	config.DB.AutoMigrate(
+		// Add all the migration structs here
+		&myuser.User{},
+	)
+}
--- internal/user/models.go
+++ internal/user/models.go
@@ -1,1 +1,19 @@
-package user
+package myuser
+
+import (
+    "gorm.io/gorm"
+    "time"
+)
+
+type User struct {
+    gorm.Model
+    FirstName      string         `gorm:"not null"`
+    LastName       string         `gorm:"not null"`
+    PhoneNumber    string         `gorm:"unique;not null"`
+    Email          string         `gorm:"unique;not null"`
+    Password       string         `gorm:"not null"`
+    Address        string         `gorm:"not null"`
+    OTP            string         
+    OTPExpiresAt   time.Time      
+    
+}
