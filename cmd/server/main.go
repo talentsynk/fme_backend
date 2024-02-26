dc71344c75afc336297bddb88f0d9f40e6181a7d
delsahim <delsahim@gmail.com>
2024-02-26 17:10:24 +0100

    setup the fme/create endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,21 @@
-package server
+package main
+
+import (
+	"fme_backend/internal/config"
+	myuser "fme_backend/internal/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+    config.ConnectToDb()
+}
+
+func main() {
+	r := gin.Default()
+
+    fme := r.Group("/fme")
+    fme.POST("/create", myuser.CreateFme)
+
+    r.Run(":8080")
+}
--- internal/user/controllers.go
+++ internal/user/controllers.go
@@ -1,33 +1,28 @@
 package myuser
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"fme_backend/internal/config"
 	"fme_backend/internal/utilities"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 )
 
 
-
-// @Summary Create a new user
-// @Description Create a new user with the provided details
-// @Tags users
-// @Accept json
-// @Produce json
-// @Param body body UserCreateSchema true "User details"
-// @Success 200 {object} User
-// @Failure 400 {object} ErrorResponse
-// @Router /users [post]
-func CreateFme(c * gin.Context) {
+func CreateFme(c *gin.Context) {
 	//Read the request body and binds it to the schema variable
+	fmt.Println("coltroller started")
 	if c.Bind(&UserCreateSchema) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read request body",
 		})
 		return
 	}
+	fmt.Println("binded succesfully")
 
 	// Check that the phone number is of the right syntax
 	if !utilities.IsNigerianPhoneNumber(UserCreateSchema.PhoneNumber){
@@ -36,6 +31,8 @@ func CreateFme(c * gin.Context) {
 		})
 		return
 	}
+
+	fmt.Println(UserCreateSchema.PhoneNumber)
 
 
 	// Hash the password
@@ -47,16 +44,20 @@ func CreateFme(c * gin.Context) {
 		})
 		return
 	}
+
+	fmt.Println("hash succesful")
 	// setup the user create instance
 
 	user:= User{
 		FirstName: UserCreateSchema.FirstName,
-		LastName: UserCreateSchema.PhoneNumber,
+		LastName: UserCreateSchema.LastName,
 		PhoneNumber: UserCreateSchema.PhoneNumber,
 		Email: UserCreateSchema.Email,
 		Password: string(hash),
-
+		OTPExpiresAt: time.Now(),
 	}
+
+	fmt.Println(user)
 
 	result := config.DB.Create(&user)
 	if result.Error != nil {
@@ -65,5 +66,7 @@ func CreateFme(c * gin.Context) {
 		})
 		return
 	}
+
+	c.JSON(200, gin.H{"message": "User created successfully"})
 }
 
