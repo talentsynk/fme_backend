3c2d2ca6305309b281f4f1ce47622380d91b15b0
Aniki Yusuf <89008095+anikiyusuf@users.noreply.github.com>
2024-04-01 21:04:54 +0100

    Revert "fix:mda"

--- internal/stc/models.go
+++ internal/stc/models.go
@@ -1,19 +1,10 @@
-package
-import (
-	"gorm.io/gorm"
-)
+package stc
 
-type Stc struct{
+import "gorm.io/gorm"
+
+type Stc struct {
+	//complete it
 	gorm.Model
-
-	ID uint
-	Ownership                    string  `gorm:"unique;not null"`
-	CentreCode                   string  `gorm:"unique;not null"`
-	Name                         string  `gorm:"unique;not null"`
-	LocalGovernment              string  `gorm:"unique;not null"`
-	State                        string  `gorm:"unique;not null"`
-	isOperational                string  `gorm:"unique;not null"`
-	CertificateOfOperationURL    string `gorm:"unique;not null"`
-	MdaID                        uint
-	UserID                       uint	
+	UserID uint
+	MdaID uint
 }
