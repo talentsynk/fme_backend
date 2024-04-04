de96db91e75fc8d518d794b7d8df3723a3ce45a7
delsahim <delsahim@gmail.com>
2024-04-04 20:49:59 +0100

    added extra create controllers

--- internal/user/controllers.go
+++ internal/user/controllers.go
@@ -400,3 +400,111 @@ func ChangePassword(c *gin.Context) {
 		"message": "Password changed succesfully",
 	})
 }
+
+func CreateMdaUser(phoneNumber, Email, Password string) (bool,string,uint){
+	var userCheck User
+	config.DB.Where("email= ?", Email).First(&userCheck)
+	if userCheck.ID != 0 {
+		
+		return false,"user already exists",0
+	}
+
+	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(Password), 10)
+	if err != nil {
+	
+		return false, "Unable to hash password",0
+	}
+
+	// setup the user create instance
+	user:= User{
+		
+		PhoneNumber: UserCreateSchema.PhoneNumber,
+		Email: UserCreateSchema.Email,
+		Password: string(hash),
+		OTPExpiresAt: time.Now(),
+		Role:2,
+		IsActive: true,
+	}
+
+	result := config.DB.Create(&user)
+	if result.Error != nil {
+		return false,"failed to create user",0
+	}
+
+
+
+	return true, "user created succesfully",user.ID
+}
+
+func CreateStcUser(phoneNumber, Email, Password string) (bool,string,uint){
+	var userCheck User
+	config.DB.Where("email= ?", Email).First(&userCheck)
+	if userCheck.ID != 0 {
+		
+		return false,"user already exists",0
+	}
+
+	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(Password), 10)
+	if err != nil {
+	
+		return false, "Unable to hash password",0
+	}
+
+	// setup the user create instance
+	user:= User{
+		
+		PhoneNumber: UserCreateSchema.PhoneNumber,
+		Email: UserCreateSchema.Email,
+		Password: string(hash),
+		OTPExpiresAt: time.Now(),
+		Role:3,
+		IsActive: true,
+	}
+
+	result := config.DB.Create(&user)
+	if result.Error != nil {
+		return false,"failed to create user",0
+	}
+
+
+
+	return true, "user created succesfully",user.ID
+}
+
+func CreateStudentUser(phoneNumber, Email, Password string) (bool,string,uint){
+	var userCheck User
+	config.DB.Where("email= ?", Email).First(&userCheck)
+	if userCheck.ID != 0 {
+		
+		return false,"user already exists",0
+	}
+
+	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(Password), 10)
+	if err != nil {
+	
+		return false, "Unable to hash password",0
+	}
+
+	// setup the user create instance
+	user:= User{
+		
+		PhoneNumber: UserCreateSchema.PhoneNumber,
+		Email: UserCreateSchema.Email,
+		Password: string(hash),
+		OTPExpiresAt: time.Now(),
+		Role:4,
+		IsActive: true,
+	}
+
+	result := config.DB.Create(&user)
+	if result.Error != nil {
+		return false,"failed to create user",0
+	}
+
+
+
+	return true, "user created succesfully",user.ID
+}
