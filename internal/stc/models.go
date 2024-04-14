6dbd7a815f202c2d3e4fa339de77536d74bfd528
Aniki Yusuf <89008095+anikiyusuf@users.noreply.github.com>
2024-04-14 07:25:23 +0100

    Update models.go

--- internal/stc/models.go
+++ internal/stc/models.go
@@ -5,13 +5,13 @@ import "gorm.io/gorm"
 type Stc struct {
 	//complete it
 	gorm.Model
-	Ownership                    string  `gorm:"unique;not null"`
-	CentreCode                   string  `gorm:"unique;not null"`
+	Ownership                    string  `gorm:"not null"`
+	CentreCode                   string  `gorm:"not null"`
 	Name                         string  `gorm:"not null"`
 	LocalGovernment              string  `gorm:"not null"`
 	State                        string  `gorm:"not null"`
 	isOperational                bool   
-	CertificateOfOperationURL    string `gorm:"unique;not null"`
+	CertificateOfOperationURL    string `gorm:"not null"`
 	Fmestc                       bool
 	MdaID                        uint
 	UserID                       uint	
