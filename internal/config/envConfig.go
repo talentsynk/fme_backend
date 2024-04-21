0379ace87634794051f1768bcf989a0bd8e1f9d8
Aniki Yusuf <89008095+anikiyusuf@users.noreply.github.com>
2024-04-21 04:15:04 +0100

    Update envConfig.go

--- internal/config/envConfig.go
+++ internal/config/envConfig.go
@@ -2,8 +2,8 @@ package config
 
 import (
 
-	"github.com/joho/godotenv"
-  "log"
+	// "github.com/joho/godotenv"
+ //  "log"
 
 
     "os"
@@ -18,20 +18,20 @@ type Config struct {
 
 var AppConfig *Config
 
-func init() {
-    // Load .env file
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+// func init() {
+//     // Load .env file
+//     err := godotenv.Load()
+//     if err != nil {
+//         log.Fatal("Error loading .env file")
+//     }
 
-    // Initializing AppConfig with values from .env file
-    AppConfig = &Config{
-        DatabaseURL: os.Getenv("DATABASE_URL"),
-        HashSecret: os.Getenv("HASH_SECRET"),
+//     // Initializing AppConfig with values from .env file
+//     AppConfig = &Config{
+//         DatabaseURL: os.Getenv("DATABASE_URL"),
+//         HashSecret: os.Getenv("HASH_SECRET"),
         
-    }
-}
+//     }
+// }
 
 
 func GetDatabaseURL() string {
