97bd5320e9b1cdf4258ecd6248c8828a9282c804
Aniki Yusuf <89008095+anikiyusuf@users.noreply.github.com>
2024-04-25 01:16:09 +0100

    Update envConfig.go

--- internal/config/envConfig.go
+++ internal/config/envConfig.go
@@ -20,20 +20,20 @@ import (
 
 // var AppConfig *Config
 
-func init() {
-    // Load .env file
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+// func init() {
+//     // Load .env file
+//     err := godotenv.Load()
+//     if err != nil {
+//         log.Fatal("Error loading .env file")
+//     }
 
-    // Initializing AppConfig with values from .env file
-    AppConfig = &Config{
-        DatabaseURL: os.Getenv("DATABASE_URL"),
-        HashSecret: os.Getenv("HASH_SECRET"),
+//     // Initializing AppConfig with values from .env file
+//     AppConfig = &Config{
+//         DatabaseURL: os.Getenv("DATABASE_URL"),
+//         HashSecret: os.Getenv("HASH_SECRET"),
         
-    }
-}
+//     }
+// }
 
 
 func GetDatabaseURL() string {
