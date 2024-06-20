2b8eaae4f2bd3d26e8958d7ed70362b96bf9321b
delsahim <delsahim@gmail.com>
2024-06-20 13:47:28 +0100

    setup for download

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	  "gorm.io/driver/postgres"
+	 "gorm.io/driver/postgres"
 	  "log"
 
 	//   "gorm.io/driver/mysql"
@@ -12,8 +12,8 @@ var DB *gorm.DB
 
 func ConnectToDb() {
 	var err error
-	//   DB, err = gorm.Open(mysql.Open(GetDatabaseURL()), &gorm.Config{})
 	  DB, err = gorm.Open(postgres.Open(GetDatabaseURL()), &gorm.Config{})
+	//   DB, err = gorm.Open(postgres.Open(GetDatabaseURL()), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error to connect to database")
 	}
