2ada0ae06f85ac5d2c3e2d2f04a54892bd957b07
Yusuf Aniki <anikiyusuf99@gmail.com>
2024-12-03 10:07:14 +0100

    fix:config

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,9 +1,9 @@
 package config
 
 import (
-	    // "gorm.io/driver/postgres"
+	     "gorm.io/driver/postgres"
 	     "log"
-        "gorm.io/driver/mysql"
+        // "gorm.io/driver/mysql"
 	    "gorm.io/gorm"
 )
 
@@ -11,8 +11,8 @@ var DB *gorm.DB
 
 func ConnectToDb() {
 	var err error
-	      DB, err = gorm.Open(mysql.Open(GetDatabaseURL()), &gorm.Config{})
-	        //  DB, err = gorm.Open(postgres.Open("postgresql://fme_db_backend_qom2_user:V3FxYabrqqMjMDQPklrxIeiwdtHeLBa7@dpg-cr5mnb2j1k6c739762hg-a.oregon-postgres.render.com/fme_db_backend_qom2"), &gorm.Config{})
+	    //   DB, err = gorm.Open(mysql.Open(GetDatabaseURL()), &gorm.Config{})
+	     DB, err = gorm.Open(postgres.Open("postgresql://fme_db_backend_qom2_user:V3FxYabrqqMjMDQPklrxIeiwdtHeLBa7@dpg-cr5mnb2j1k6c739762hg-a.oregon-postgres.render.com/fme_db_backend_qom2"), &gorm.Config{})
          	if err != nil {
 	     	log.Fatal("Error to connect to database")
     	}
